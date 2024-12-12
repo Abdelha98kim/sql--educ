ea7ae5b287c81ae7bb6a62b6a158fc8469b987be
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2024-12-12 15:42:26 +0100

    Ajouter des requets

--- sql/db.sql
+++ sql/db.sql
@@ -59,7 +59,34 @@ CREATE TABLE sponsors (
     montant NUMERIC(10, 2) NOT NULL
 );
 
-
 INSERT INTO sponsors VALUES (1, "simplon", "simploncont", "simplon.co@gmail.com", "0000000000", 12345.12);
 
 
+
+
+-- Afficher tous les membres du BDE avec leur rôle et leur date d’adhésion.
+SELECT * FROM membres;
+
+-- Lister tous les événements prévus après une date donnée.
+SELECT nom, description FROM evenements WHERE date > "2025-01-01";
+
+-- Afficher les informations des événements ayant un budget supérieur à 5000 MAD.
+SELECT * FROM evenements WHERE budget > 5000;
+
+-- Afficher les sponsors qui ont contribué à des événements ayant un budget supérieur à 3500 MAD.
+SELECT * FROM sponsors WHERE montant > 3500;
+
+-- Compter le nombre total d’événements organisés par le BDE.
+SELECT COUNT(*) AS total_evenement FROM evenements;
+
+-- Calculer la moyenne du budget des événements organisés.
+SELECT AVG(budget) AS moyenne_budget FROM evenements;
+
+-- Lister les événements qui se déroulent dans un lieu donné.
+SELECT * FROM evenements WHERE lieu LIKE "%youcode%";
+
+-- Afficher les 5 derniers événements organisés par le BDE.
+SELECT * FROM evenements ORDER BY id DESC LIMIT 5;
+
+-- Lister les membres du BDE qui ont rejoint avant une date donnée.
+SELECT * FROM membres WHERE date_adhesion < "2025-01-01";
